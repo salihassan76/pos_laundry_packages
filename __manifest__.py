39e1f30473f45f42d38c08b6ff742d88df9ba153
Sayed Ali <sali.hassan.76@gmail.com>
2026-07-07 20:31:40 +0300

    Inital Package Setup

---
+++ __manifest__.py
@@ -0,0 +1,26 @@
+{
+    "name": "POS Laundry Packages",
+    "version": "1.0.0",
+    "depends": [
+        "pos_laundry",
+        "point_of_sale",
+    ],
+    "category": "Operations",
+    "summary": "Package sale and package usage for POS Laundry",
+    "description": """
+POS Laundry Packages
+
+Adds package functionality on top of POS Laundry:
+- Package rules
+- Customer packages
+- Package selling
+- Package usage
+- Package balance tracking
+- Package usage history
+    """,
+    "author": "Sayed Ali Hassan",
+    "website": "",
+    "license": "LGPL-3",
+    "installable": True,
+    "application": False,
+}
